fa928506fe1a3a7d4c134256c02625c18cae03ef
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-06-16 16:42:07 +0200

    FIX: Update cyanure solver

--- solvers/cyanure.py
+++ solvers/cyanure.py
@@ -1,16 +1,18 @@
+import warnings
+from sklearn.exceptions import ConvergenceWarning
 from benchopt import BaseSolver, safe_import_context
 
 
 with safe_import_context() as import_ctx:
     import scipy
-    from cyanure import BinaryClassifier
+    from cyanure.estimators import Classifier
 
 
 class Solver(BaseSolver):
-    name = 'Cyanure'
+    name = 'cyanure.estimators'
 
     install_cmd = 'conda'
-    requirements = ['mkl', 'pip:cyanure-mkl']
+    requirements = ['cyanure']
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
@@ -18,16 +20,22 @@ class Solver(BaseSolver):
                 scipy.sparse.isspmatrix_csc(self.X)):
             self.X = scipy.sparse.csr_matrix(self.X)
 
-        self.solver = BinaryClassifier(loss='logistic', penalty='l1',
-                                       fit_intercept=False)
+        warnings.filterwarnings('ignore', category=ConvergenceWarning)
+
+
         self.solver_parameter = dict(
-            lambd=self.lmbd / self.X.shape[0], solver='auto', it0=1000000,
-            tol=1e-12, verbose=False
+        lambda_1=self.lmbd / self.X.shape[0], solver='auto', duality_gap_interval=10,
+        tol=1e-12, verbose=False
         )
 
-    def run(self, n_iter):
-        self.solver.fit(self.X, self.y, max_epochs=n_iter,
+        self.solver = Classifier(loss='logistic', penalty='l1',
+                                       fit_intercept=False,
                         **self.solver_parameter)
+       
+
+    def run(self, n_iter):
+        self.solver.max_iter = n_iter
+        self.solver.fit(self.X, self.y)
 
     def get_result(self):
         return self.solver.get_weights()
